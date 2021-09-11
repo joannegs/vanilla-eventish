1940ea629fd0c1092d6b424d599d0f8e153b5c33
Joanne Gabriela <joanneegabriela@gmail.com>
2021-09-11 15:50:03 -0300

    Atualização nas rotas da aplicação

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const db = require("./database/db.js");
 
 const server = express();
 
+server.use(express.static("public"))
+
 server.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure("src/views", {
@@ -14,6 +16,10 @@ nunjucks.configure("src/views", {
 server.get("/", (req, res) => {
     return res.render("index.html");
 });
+
+server.get("/sign-up", (req, res) => {
+    return res.render("sign-up.html")
+})
 
 
 server.listen(3000);
