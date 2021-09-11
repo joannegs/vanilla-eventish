af66ced29f2826ec2d63bf05092e28601f7da270
Joanne Gabriela <joanneegabriela@gmail.com>
2021-09-11 19:18:09 -0300

    Implementado o cadastro de usuários

--- src/database/db.js
+++ src/database/db.js
@@ -2,3 +2,15 @@ const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./src/database/database.db");
 
 module.exports = db;
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            email TEXT,
+            password TEXT
+        );
+    `);
+});
+
--- src/server.js
+++ src/server.js
@@ -4,12 +4,15 @@ const db = require("./database/db.js");
 
 const server = express();
 
+server.use(express.json())
+server.use(express.urlencoded({ extended: true }))
+
 server.use(express.static("public"))
 
 server.use(express.urlencoded({ extended: true }));
 
 nunjucks.configure("src/views", {
-    express: server, 
+    express: server,
     noCache: true
 });
 
@@ -19,8 +22,23 @@ server.get("/", (req, res) => {
 
 server.get("/sign-up", (req, res) => {
     return res.render("sign-up.html")
-})
+});
 
+server.post("/save-user", (req, res) => {
+    const query = `INSERT INTO users (name, email, password) VALUES (?, ?, ?);`;
+    const values = [req.body.name, req.body.email, req.body.password];
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.status(500).send("An error occurred while trying to sign-up");
+        }
+        // redirecionar para a pagina inicial
+        return res.status("Ok, it worked");
+    }
+
+    db.run(query, values, afterInsertData)
+});
 
 server.listen(3000);
 
