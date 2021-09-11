c51e83f2755a3caf6beaeae771fe1ed02cc2af63
Joanne Gabriela <joanneegabriela@gmail.com>
2021-09-11 19:23:41 -0300

    Implementado o login de usuários

--- src/server.js
+++ src/server.js
@@ -40,5 +40,24 @@ server.post("/save-user", (req, res) => {
     db.run(query, values, afterInsertData)
 });
 
+server.post("/login", (req, res) => {
+    const query = `SELECT * FROM users WHERE email = ?;`;
+    const values = [req.body.email];
+
+    function afterInsertData(err) {
+        if (err) res.status(500).send("Intern error");
+    }
+
+    db.get(query, values, (err, row) => {
+        afterInsertData(err);
+
+        if (row) {
+            row.password === req.body.password ? res.status(200).send("logado") : res.status(403).send("não logado")
+        } else {
+            res.status(404).send("não existe registro");
+        }
+    });
+});
+
 server.listen(3000);
 
