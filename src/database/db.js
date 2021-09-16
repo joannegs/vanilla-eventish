0773c215218825050786be7dfcb1e3cb6eba68e2
Joanne Gabriela <joanneegabriela@gmail.com>
2021-09-16 02:46:10 -0300

    Atualização na declaração das tabelas do DB

--- src/database/db.js
+++ src/database/db.js
@@ -12,5 +12,17 @@ db.serialize(() => {
             password TEXT
         );
     `);
+
+    db.run(`
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        date DATE TEXT,
+        initial_hour TEXT,
+        final_hour TEXT,
+        user_id INTEGER, 
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    );
+`);
 });
 
